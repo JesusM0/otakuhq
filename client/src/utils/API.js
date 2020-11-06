28d2d284bf3850b04c93b266b2d3bf509025dfba
Junioresc <junioresc1092@gmail.com>
2020-11-06 17:01:51 -0500

    changed fetch requests to functions and implemented apolloprovider

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,20 +5,19 @@ export const searchJikanApi = (query) => {
 };
 
 // get Trending anime from Kitsu
-export const getKitsuTrending = fetch(
-  'https://kitsu.io/api/edge/trending/anime'
-);
+export const getKitsuTrending = () =>
+  fetch('https://kitsu.io/api/edge/trending/anime');
 
 // brings up list of all the anime not sorted, at the bottom there is links to first, next, last and are used to see more anime's
 // 10 anime's per response
-export const getKitsuAnime = fetch('https://kitsu.io/api/edge/anime');
+export const getKitsuAnime = () => fetch('https://kitsu.io/api/edge/anime');
 
 // has a max response of 10 categories, clicking on relationships => anime => related brings up anime of that category and
 // at the bottom there is links to first, next, last and are used to see more categories
-export const getKitsuCategories = fetch('https://kitsu.io/api/edge/categories');
+export const getKitsuCategories = () =>
+  fetch('https://kitsu.io/api/edge/categories');
 
 // when getting the response, go to category => links => related to get the type of category
 // then clicking on relationships => anime => related brings up anime's of that category
-export const getCategoryFavorites = fetch(
-  'https://kitsu.io/api/edge/category-favorites'
-);
+export const getCategoryFavorites = () =>
+  fetch('https://kitsu.io/api/edge/category-favorites');
