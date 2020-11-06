f67cc55d0cb838c3b49d1cbd0253789eddd1b60c
Kos-Mos <ChuyM203@gmail.com>
2020-11-05 20:06:20 -0500

    final boilerplate code

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,7 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID
-    userName: String
+    username: String
     email: String
   }
 
@@ -21,7 +21,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addUser(userName: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
   }
 `;
