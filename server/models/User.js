8c77604a17c32ebd9864537a1db2f6a23f191b09
Kos-Mos <ChuyM203@gmail.com>
2020-11-05 19:48:09 -0500

    more boilerplate for client

--- server/models/User.js
+++ server/models/User.js
@@ -4,29 +4,26 @@ const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-  firstName: {
+  username: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
+    match: /[\S]+/,
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    match: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}/,
   },
   password: {
     type: String,
     required: true,
-    minlength: 5,
+    minlength: 8,
   },
 });
 
-// set up pre-save middleware to create password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
@@ -36,7 +33,6 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-// compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,8 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID
-    firstName: String
-    lastName: String
+    userName: String
     email: String
-    orders: [Order]
   }
 
   type Checkout {
@@ -20,22 +18,10 @@ const typeDefs = gql`
 
   type Query {
     user: User
-    checkout(products: [ID]!): Checkout
   }
 
   type Mutation {
-    addUser(
-      firstName: String!
-      lastName: String!
-      email: String!
-      password: String!
-    ): Auth
-    updateUser(
-      firstName: String
-      lastName: String
-      email: String
-      password: String
-    ): User
+    addUser(userName: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
   }
 `;
